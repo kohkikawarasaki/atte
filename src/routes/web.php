7081dc26f56987bc9044f68d6a5552e503bbf5ac
kohkikawarasaki <52435129+kohkikawarasaki@users.noreply.github.com>
2024-05-20 05:42:19 +0900

    Revert "feat:auth"

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\StampController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth')->group(function () {
-    Route::get('/', [StampController::class, 'index']);
-    Route::post('/', [StampController::class, 'store']);
-    Route::get('/attendance', [StampController::class, 'attendance']);
+Route::get('/', function () {
+    return view('welcome');
 });
-
